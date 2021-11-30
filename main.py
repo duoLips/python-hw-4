f4427ddc4c78a7114457b5b4b96e9a77d7db2fd7
duoLips <djoneshargo@gmail.com>
2021-11-30 23:57:26 +0200

    Add file

---
+++ main.py
@@ -0,0 +1,34 @@
+"""ex 2"""
+
+# my_string = input("Input text to reverse")
+# for char in  range(len(my_string) - 1, - 1, - 1):
+#     print(my_string[char])
+
+"""ex 3"""
+
+# a = int(input("Enter a: "))
+# for i in range (0, a):
+#     print("*" * i)
+# print("*"*a)
+# for _ in range (0, a):
+#     a -= 1
+#     print("*" * a)
+
+"""ex 4"""
+
+# A = int(input("Enter A: "))
+# B = int(input("Enter B: "))
+# if (A < B):
+#     for i in range(A, B+1):
+#         print(i)
+# if (A > B):
+#     for i in range (A, B - 1, - 1):
+#         print (i)
+
+""" ex 5"""
+
+# A = int(input("Enter A: "))
+# B = int(input("Enter B: "))
+# if (A < B):
+#     for i in range(A, B+1):
+#         print(i)
